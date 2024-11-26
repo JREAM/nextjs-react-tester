d0e3e76c094ad0ebd57f79bd5c17aaaedd79b2b1
Jesse Boyer <imboyus@gmail.com>
2024-11-25 20:49:59 -0500

    basic logic done, make it pretty a lil later, ez peezy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+"use client"
+import useCompanies from '@/lib/hooks'
+
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from "@/components/ui/card"
 
+
+// TODO: Convert to table, adjust styles, dble check instructions, organize files a lil diff
 export default function Home() {
+
+  const { data, isLoading, error } = useCompanies()
+
   return (
     <div className="min-h-screen min-w-screen bg-gradient-to-tl from-blue-400 to-blue-700 text-white space-y-6">
       <div className="bg-white/10 p-4">
@@ -22,9 +30,27 @@ export default function Home() {
             <CardTitle></CardTitle>
             <CardDescription></CardDescription>
           </CardHeader>
-          <CardContent>{/* content goes here */}</CardContent>
+          <CardContent>
+            <ul>
+              {isLoading && <li>Loading</li>}
+              {error && <li>Error: {error}</li>}
+              {data?.map((company) => (
+                <li key={company.id}>
+                  <strong>{company.name}</strong> - {company.description}, {company.location}
+                  {company.website} {company.revenue} {company.employees}
+                </li>
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            Total Employees: {data?.reduce((total, item) => total + item.employees, 0)}
+            Total Revenue: {data?.reduce((total, item) => total + item.revenue, 0)}
+          </CardContent>
         </Card>
       </div>
     </div>
-  );
+  )
 }
+
